5b453c914c3f5762255975288d1cf3355492b98e
Martin Lundfall <martin.lundfall@protonmail.com>
2020-04-16 12:08:42 +0200

    Small changes for better `act` integration

--- src/hevm/src/EVM/ABI.hs
+++ src/hevm/src/EVM/ABI.hs
@@ -65,7 +65,6 @@ import Data.Text.Encoding (encodeUtf8)
 import Data.Vector        (Vector)
 import Data.Word          (Word32, Word8)
 import Data.List          (intercalate)
-import Data.SBV hiding (label)
 import GHC.Generics
 
 import Test.QuickCheck hiding ((.&.), label)
@@ -118,7 +117,10 @@ data AbiType
   | AbiArrayDynamicType AbiType
   | AbiArrayType        Int AbiType
   | AbiTupleType        (Vector AbiType)
-  deriving (Show, Read, Eq, Ord, Generic)
+  deriving (Read, Eq, Ord, Generic)
+
+instance Show AbiType where
+  show = Text.unpack . abiTypeSolidity
 
 data AbiKind = Dynamic | Static
   deriving (Show, Read, Eq, Ord, Generic)
